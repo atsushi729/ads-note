9484cf2deb9a8f37e38e9a323f0372a55507cbdf
Atsushi <atu729@icloud.com>
2026-06-28 13:42:12 +0900

    feat: shared Markdown renderer with math + gfm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "katex/dist/katex.min.css";
 import { Hanken_Grotesk, JetBrains_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 const hanken = Hanken_Grotesk({ subsets: ["latin"], weight: ["400","500","600","700","800"], variable: "--font-hanken" });
